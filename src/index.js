128629e0380689bb4b802b9f4d9fb831928730b0
Russell Lin <superbr7789@gmail.com>
2023-12-21 07:26:43 +0800

    add third parties libraries

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 /* eslint-disable quotes */
 import React from "react";
-import ReactDom from "react-dom";
+// import ReactDom from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import App from "./components/App";
 
-ReactDom.render(<App />, document.getElementById("root"));
+const container = document.getElementById("root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
+
+// ReactDom.render(<App />, document.getElementById("root"));
