f6500024118797ad5e55be90c20b7bae9aaf0eb6
Russell Lin <superbr7789@gmail.com>
2023-12-30 19:22:19 +0800

    add basic settings for movie info page

--- src/components/MovieInformation/MovieInformation.jsx
+++ src/components/MovieInformation/MovieInformation.jsx
@@ -1,10 +1,77 @@
 /* eslint-disable no-console */
 /* eslint-disable quotes */
 import React from "react";
+import {
+  Modal,
+  Typography,
+  Button,
+  ButtonGroup,
+  Grid,
+  Box,
+  CircularProgress,
+  useMediaQuery,
+  Rating,
+} from "@mui/material";
+import {
+  Movie as MovieIcon,
+  Theaters,
+  Language,
+  PlusOne,
+  Favorite,
+  FavoriteBorderOutlined,
+  Remove,
+  ArrowBack,
+} from "@mui/icons-material";
+import { Link, useParams } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import axios from "axios";
+
+import { useGetMovieQuery } from "../../services/TMDB";
+import useStyles from "./styles";
 
 const MovieInformation = () => {
   console.log("Movie");
-  return <div>MovieInformation</div>;
+  const { id } = useParams();
+  const { data, isFetching, error } = useGetMovieQuery(id);
+  const classes = useStyles();
+
+  if (isFetching) {
+    return (
+      <Box display="flex" justifyContent="center">
+        <CircularProgress size="8rem" />
+      </Box>
+    );
+  }
+
+  if (error) {
+    return <Link to="/">Something has gone wrong, go back.</Link>;
+  }
+
+  return (
+    <Grid container className={classes.containerSpaceAround}>
+      <Grid item sm={12} lg={4}>
+        <img
+          className={classes.poster}
+          src={`https://image.tmdb.org/t/p/w500/${data?.poster_path}`}
+          alt={data?.title}
+        />
+      </Grid>
+      <Grid item container direction="column" lg={7}>
+        <Typography variant="h3" align="center" gutterBottom>
+          {data?.title} ({data.release_date.split("-")[0]})
+        </Typography>
+        <Typography variant="h5" align="center" gutterBottom>
+          {/* use question mark if you are not sure if you gonna get that property */}
+          {data?.tagline}
+        </Typography>
+        <Grid item className={classes.containerSpaceAround}>
+          <Box display="flex" align="center">
+            <Rating readOnly value={data.vote_average / 2} />
+          </Box>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default MovieInformation;
